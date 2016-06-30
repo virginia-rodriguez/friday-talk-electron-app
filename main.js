c7365c19aa560dfc87d9981153b95071060283be
Virginia Rodriguez <virginia.rodriguez@moove-it.com>
2016-06-30 15:06:49 -0300

    Add basic menu

--- main.js
+++ main.js
@@ -3,10 +3,22 @@
 const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 
 let mainWindow = null;
+let menuTemplate = [
+  {
+    label: 'Friday Talk',
+    submenu: [
+      { label: 'Preferences' },
+      { label: 'Quit', accelerator: 'Command+Q', click: app.quit }
+    ]
+  }];
 
 app.on("ready", function() {
+  var menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
   mainWindow.loadURL("file://" + __dirname + "/index.html");
 });
